ae9a7045656cb7361ff361c1ac0657a100b5594c
leonardo fleitas <leoflalv.dev@gmail.com>
2024-08-25 17:45:10 +0200

    solve challenge 2

---
+++ unique-id-generator-challenge/src/handler.rs
@@ -0,0 +1,22 @@
+use async_trait::async_trait;
+use maelstrom::{done, protocol::Message, Node, Result, Runtime};
+use ulid::Ulid;
+
+pub struct Handler {}
+
+#[async_trait]
+impl Node for Handler {
+    async fn process(&self, runtime: Runtime, req: Message) -> Result<()> {
+        match req.get_type() {
+            "generate" => {
+                let ulid = Ulid::new();
+                let new_id = ulid.to_string();
+                let mut msg = req.body.clone().with_type("generate_ok");
+                msg.extra.insert("id".into(), new_id.into());
+
+                runtime.reply(req, msg).await
+            }
+            _ => done(runtime, req),
+        }
+    }
+}
