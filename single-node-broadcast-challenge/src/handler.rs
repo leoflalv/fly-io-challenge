cd4dac00a1aa289f55d6fd839a72c52bd9c3afa7
leonardo fleitas <leoflalv.dev@gmail.com>
2024-08-28 19:22:12 +0200

    store and return messages

--- single-node-broadcast-challenge/src/handler.rs
+++ single-node-broadcast-challenge/src/handler.rs
@@ -1,16 +1,41 @@
+use std::sync::{Arc, Mutex};
+
 use async_trait::async_trait;
 use maelstrom::{done, protocol::Message, Node, Result, Runtime};
+use serde_json::{json, Value};
 
 #[derive(Clone, Default)]
-pub struct Handler {}
+pub struct Handler {
+    store: Arc<Mutex<Vec<Value>>>,
+}
 
 #[async_trait]
 impl Node for Handler {
     async fn process(&self, runtime: Runtime, req: Message) -> Result<()> {
         match req.get_type() {
-            "generate" => {
-                unimplemented!()
+            "broadcast" => {
+                let store = self.store.clone();
+                let message = req.body.extra.get("message").unwrap().clone();
+
+                {
+                    let mut data = store.lock().unwrap();
+                    data.push(message);
+                }
+
+                runtime.reply_ok(req).await
             }
+            "read" => {
+                let store = self.store.clone();
+                let mut resp = Message::default().body.with_type("read_ok");
+                {
+                    let data = store.lock().unwrap();
+                    let array = json!(*data);
+                    resp.extra.insert("messages".into(), array);
+                }
+
+                runtime.reply(req, resp).await
+            }
+            "topology" => runtime.reply_ok(req).await,
             _ => done(runtime, req),
         }
     }
